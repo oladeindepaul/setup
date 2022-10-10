6a660be24c2208d1913e636419f08dcf7b1f6a35
oladeindepaul <101461115+oladeindepaul@users.noreply.github.com>
2022-10-10 08:14:32 +0100

    Update settings.py

--- songcrud/settings.py
+++ songcrud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4aty-*qvm2fgt40c3w4tk1l@nweg1e0x8s!s@uub*@_jp*5%r2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
