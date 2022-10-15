4550a57c1221f8b525073c91e571b3031e1b5c21
oladeindepaul <anuoluwapooladeinde@gmail.com>
2022-10-15 04:07:32 +0100

    good day

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length =50)
+    last_name = models.CharField(max_length = 50)
+    artiste_age = models.CharField(max_length = 30)
+
+
+class Song(models.Model):
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length =100)
+    release_date = models.DateField()
+    likes = models.CharField(max_length = 100)
+    artiste_id = models.CharField(max_length =100)
+
+
+class Lyrics(models.Model):
+    content = models.CharField(max_length =100)
+    song_id = models.CharField(max_length =100)
