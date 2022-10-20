88744e4a6dbc84a16113087a76952e41b0a08ab3
oladeindepaul <anuoluwapooladeinde@gmail.com>
2022-10-20 14:17:37 +0100

    Good commit

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from musicapp.models import Artiste, Song, Lyrics
 
 # Register your models here.
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyrics)
